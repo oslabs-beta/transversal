aaeb933fecbcea958b142e07913d669b36bb8435
Daniel Forrester <danielf216@gmail.com>
2022-02-12 17:49:03 -0500

    Feature: added concurrently script for dev testing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use('/', (req, res) => res.status(200).send('<h2>Transversal App</h2>'));
 
