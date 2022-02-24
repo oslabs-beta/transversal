145c3c63263c7cee2273140e48b0fd961e2fd95b
Daniel Forrester <danielf216@gmail.com>
2022-02-23 21:07:45 -0500

    Feature: Redis configuration in server.js to set the URI and PORT for the redis server

--- sample/TransversalCache.js
+++ sample/TransversalCache.js
@@ -3,12 +3,21 @@ const Transversal = require('./Transversal');
 const fetch = require('node-fetch');
 
 class TransversalCache {
-	constructor() {
-		this.client = (() => {
-			const client = redis.createClient();
-			client.connect();
-			return client;
-		})();
+	constructor(redisClient) {
+		
+		/**
+		 * Connect Redis Client
+		 */
+		this.client = redisClient;
+		this.client.connect();
+		this.client.on('error', (err) => console.log('Redis Client Error', err));
+
+		/**
+		 * Middleware
+		 * @param {*} req 
+		 * @param {*} res 
+		 * @returns 
+		 */
 		this.cacheMiddleware = async (req, res) => {
 			const request = async (endpoint, gql, variables) => {
 				const res = await fetch(endpoint, {
