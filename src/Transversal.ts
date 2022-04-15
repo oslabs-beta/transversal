d55dcab35f629637f3813b409cafe700decad9b0
han900204 <54014070+han900204@users.noreply.github.com>
2022-04-14 18:56:05 -0700

    updates

--- src/Transversal.ts
+++ src/Transversal.ts
@@ -1,13 +1,13 @@
 export {};
 
-const Cache = require('../cache/Cache');
+const TransversalCache = require('./cache/TransversalCache');
 
 class Transversal extends require('./utilities/GQLStringGenerator') {
   public cache: any;
 
   constructor(MongoModels: any[], redisClient: any) {
     super(MongoModels);
-    this.cache = new Cache(redisClient);
+    this.cache = new TransversalCache(redisClient);
   }
 }
 
