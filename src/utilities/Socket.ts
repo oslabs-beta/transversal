064689d96dba5e48ad0db657c114ce756aead307
han900204 <54014070+han900204@users.noreply.github.com>
2022-04-14 18:51:45 -0700

    updates

--- src/utilities/Socket.ts
+++ src/utilities/Socket.ts
@@ -1,41 +1,42 @@
-import { io, Socket } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 class TransversalSocket {
-	constructor(url: string) {
-		this.socket: any = io(url)
-	}
-	/**
-	 * Transversal Instance
-	 * @param {*} timeout The timeout for socket connection
-	 * @returns
-	 */
-	getTransversalInstance(timeout: number = 10000) {
-		return new Promise((resolve, reject) => {
-			// eslint-disable-next-line prefer-const
-			let timer: any
+	public socket: any
 
-			function responseHandler(data: any) {
+	constructor(url) {
+		this.socket = io(url);
+	}
+
+	getTransversalInstance(timeout = 10000) {
+		return new Promise((resolve, reject) => {
+			let timer;
+
+			function responseHandler(data) {
 				data = JSON.parse(data, (name, val) => {
-					if (val && typeof val === 'string' &&	(val.startsWith('function') || val.startsWith('async'))) {
-						return new Function('return ' + val)()
+					if (
+						val &&
+						typeof val === 'string' &&
+						(val.startsWith('function') || val.startsWith('async'))
+					) {
+						return new Function('return ' + val)();
 					} else {
-						return val
+						return val;
 					}
-				})
-				resolve(data)
-				clearTimeout(timer)
+				});
+				resolve(data);
+				clearTimeout(timer);
 			}
 
-			this.socket.on('connect', () => console.log(this.socket.id))
+			this.socket.on('connect', () => console.log(this.socket.id));
 
-			this.socket.once('transverse', responseHandler)
+			this.socket.once('transverse', responseHandler);
 
-			timer = setTimeout((): void => {
-				reject(new Error('Timeout waiting for response from the server'))
-				socket.removeListener('transverse', responseHandler)
-			}, timeout)
+			timer = setTimeout(() => {
+				reject(new Error('Timeout waiting for response from the server'));
+				this.socket.removeListener('transverse', responseHandler);
+			}, timeout);
 		});
 	}
 }
 
-export default TransversalSocket
+export default TransversalSocket;
