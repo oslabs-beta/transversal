3d3a92b9f3a14962c36e695c9d7dd3fda3ef15ac
han900204 <54014070+han900204@users.noreply.github.com>
2022-05-04 17:36:50 -0700

    updates

--- src/TransversalClient.ts
+++ src/TransversalClient.ts
@@ -34,6 +34,59 @@ class TransversalClient {
       }, timeout);
     });
   }
+  async transversalQuery(gql, variables, cacheOption = false, custom) {
+    if (custom) {
+      const pattern = /^.+{$/gm;
+
+      const queryString = pattern.exec(gql);
+      const queryString2 = pattern.exec(gql);
+
+      gql = queryString + '\n' + queryString2 + '\n' + custom + '\n' + '}' + '\n' + '}';
+    }
+
+    const request = async (endpoint, gql, variables) => {
+      const res = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          query: gql,
+          variables: variables,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => data);
+      return res;
+    };
+
+    console.log('Checking cache option..!', gql);
+
+    if (!cacheOption) {
+      console.log('caching option not selected');
+      const res = await request('/graphql', gql, variables);
+      console.log('res', res);
+      return res;
+    } else {
+      console.log('caching option selected!');
+
+      const res = await fetch('/transversal', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          query: gql,
+          variables: variables,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => data);
+      return res;
+    }
+  }
 }
 
 export default TransversalClient;
